e41456fcb59c557b4a9fd4ff2d815c3687935c14
Mark Heckler <mark.heckler@gmail.com>
2021-09-09 15:02:28 -0500

    Polishing

--- src/main/java/com/thehecklers/planefinder/PlanefinderApplication.java
+++ src/main/java/com/thehecklers/planefinder/PlanefinderApplication.java
@@ -1,6 +1,5 @@
 package com.thehecklers.planefinder;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Hooks;
@@ -8,14 +7,6 @@ import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class PlanefinderApplication {
-//	@Value("${planeip:192.168.1.249}")
-//	private String planeIp;
-//
-//	@Bean
-//	WebClient client() {
-//		return WebClient.create("http://" + planeIp + "/ajax/aircraft");
-//	}
-
 	public static void main(String[] args) {
 		//ReactorDebugAgent.init();
 		Hooks.onErrorDropped(e -> System.out.println("\n<< Client disconnected >>\n"));
